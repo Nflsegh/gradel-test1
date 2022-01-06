94fb5d06a9b1ff1c8f9dca7ccf8bc9b66cb6d009
Nflsegh <92489755+Nflsegh@users.noreply.github.com>
2022-01-06 11:45:26 -0600

    add file

---
+++ src/main/java/com/practice/myGradelApp/MyGradelAppApplication.java
@@ -0,0 +1,13 @@
+package com.practice.myGradelApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGradelAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGradelAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/myGradelApp/MyGradelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.myGradelApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGradelAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
